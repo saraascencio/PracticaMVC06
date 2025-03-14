d0c015ed6aa4d88a9f7a3688d414f52ae72937bc
saraascencio <sara.ascencio1@catolica.edu.sv>
2025-03-13 18:07:25 -0600

    Seguimiento y finalización de guía

---
+++ PracticaMVC/Controllers/vistaRazorController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PracticaMVC.Controllers
+{
+    public class vistaRazorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult bloqueMultiple()
+        {
+            return View();
+        }
+
+        public IActionResult condiciones()
+        {
+            return View();
+        }
+
+        public IActionResult bucles()
+        {
+            return View();
+        }
+    }
+}
